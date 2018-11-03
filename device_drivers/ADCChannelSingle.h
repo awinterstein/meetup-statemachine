18e525481b3d5fe922d69774a8ca0f89aacf17e0
Eugen Wist <eugen.wist@zuehlke.com>
2018-11-03 22:48:08 +0100

    small not yet working implementation of adc

--- device_drivers/ADCChannelSingle.h
+++ device_drivers/ADCChannelSingle.h
@@ -8,7 +8,20 @@
 #ifndef DRIVERS_BUTTONOVERADC_H_
 #define DRIVERS_BUTTONOVERADC_H_
 
+#include <fsl_adc16.h>
+
 namespace Driver {
+
+template <ADC_Type * usedADC>
+class ADCChannelSingle
+{
+	static uint32_t getRawValue ()
+	{
+        ADC16_SetChannelConfig(usedADC, 0u, &adc16ChannelConfigStruct);
+        while (0U == (kADC16_ChannelConversionDoneFlag & ADC16_GetChannelStatusFlags(usedADC, 0u))) { }
+        return ADC16_GetChannelConversionValue(usedADC, 0u);
+	}
+};
 
 } /* namespace Driver */
 
