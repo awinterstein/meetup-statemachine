0fb8ef50005b89e2a5784f59d2b38888e431126e
Eugen Wist <eugen.wist@zuehlke.com>
2018-11-03 22:06:09 +0100

    created dummy files for button and adc

---
+++ device_drivers/ADCChannelSingle.h
@@ -0,0 +1,15 @@
+/*
+ * Pin.h
+ *
+ *  Created on: 03 Nov 2018
+ *      Author: meetup
+ */
+
+#ifndef DRIVERS_BUTTONOVERADC_H_
+#define DRIVERS_BUTTONOVERADC_H_
+
+namespace Driver {
+
+} /* namespace Driver */
+
+#endif /* DRIVERS_BUTTONOVERADC_H_ */
---
+++ device_drivers/ButtonOverADC.h
@@ -0,0 +1,15 @@
+/*
+ * Pin.h
+ *
+ *  Created on: 03 Nov 2018
+ *      Author: meetup
+ */
+
+#ifndef DRIVERS_ADCCHANNELSINGLE_H_
+#define DRIVERS_ADCCHANNELSINGLE_H_
+
+namespace Driver {
+
+} /* namespace Driver */
+
+#endif /* DRIVERS_ADCCHANNELSINGLE_H_ */
