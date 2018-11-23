b44f67112e3205340414b7efe0a14a8a3a3cb10e
Rove <rove@zuehlke.com>
2018-11-23 15:44:18 +0100

    start including the generated code...

--- source/generated_main.cpp
+++ source/generated_main.cpp
@@ -1,6 +1,15 @@
+#include "setup.hpp"
+
+#include "builtins_statemachine.hpp"
+#include "support_statemachine.hpp"
+
+#include "generated_state_machine_blinky.hpp"
+
 
 
 int main()
 {
+  setup();
+  finish();
   return 0;
 }
