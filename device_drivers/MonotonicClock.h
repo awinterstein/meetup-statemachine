3422fd2aa65155b0be3cc29160fb98b1ea52aaf9
Eugen Wist <eugen.wist@zuehlke.com>
2018-11-23 13:31:02 +0100

    reworked timebase functions

--- device_drivers/MonotonicClock.h
+++ device_drivers/MonotonicClock.h
@@ -19,43 +19,31 @@ template <uint32_t pitBase, clock_name_t clockSource, uint32_t secondsDowncounti
 class MonotonicClock {
 public:
 
-#if defined(FSL_FEATURE_PIT_HAS_LIFETIME_TIMER) && FSL_FEATURE_PIT_HAS_LIFETIME_TIMER
-
 	static std::chrono::microseconds microseconds()
 	{
-		const auto instance = reinterpret_cast<PIT_Type*>(pitBase);
-		const auto count	= PIT_GetLifetimeTimerCount(instance);
-		return convertLifeTimeToTime(count);
+		const auto highReg		= PIT_GetCurrentTimerCount(reinterpret_cast<PIT_Type*>(pitBase), kPIT_Chnl_1);
+		const auto lowReg		= PIT_GetCurrentTimerCount(reinterpret_cast<PIT_Type*>(pitBase), kPIT_Chnl_0);
+
+		const auto highCount	= secondsDowncountingMax - highReg;
+		const auto sec			= std::chrono::seconds{highCount};
+
+		const auto lowCount		= msGetter{}() - lowReg;
+		const auto microsec		= std::chrono::microseconds{COUNT_TO_USEC(lowCount, CLOCK_GetFreq(clockSource))};
+
+		return sec + microsec;
 	}
-
-#else
-
-	static std::chrono::microseconds microseconds()
-	{
-		const auto instance = reinterpret_cast<PIT_Type*>(pitBase);
-		const auto high		= PIT_GetCurrentTimerCount(instance, kPIT_Chnl_1);
-		const auto low		= PIT_GetCurrentTimerCount(instance, kPIT_Chnl_0);
-		const auto count	= (static_cast<uint64_t>(high) << 32) | low;
-		return convertLifeTimeToTime(count);
-	}
-
-#endif
-
 
 	static void delay (std::chrono::microseconds microToBusyWait)
 	{
 		const auto start = microseconds();
-		while (microseconds() - start < microToBusyWait) {};
-	}
-
-private:
-	static std::chrono::microseconds convertLifeTimeToTime (uint64_t liveTimeCount)
-	{
-		const auto clockFrequency	= CLOCK_GetFreq(clockSource);
-		const auto sec				= std::chrono::seconds{secondsDowncountingMax - uint32_t(liveTimeCount >> 32U)};
-		const auto lowCount			= msGetter{}() - uint32_t(0xFFFFFFFF & liveTimeCount);
-		const auto microsec			= std::chrono::milliseconds{COUNT_TO_USEC(lowCount, clockFrequency)};
-		return sec + microsec;
+		while (true)
+		{
+			const auto delta = microseconds() - start;
+			if (delta > microToBusyWait)
+			{
+				break;
+			}
+		};
 	}
 };
 
